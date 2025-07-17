a99bce29d59db88bf4a893f7ec2d98f2d70dee22
Zarnigoor <zarnigorumaraliyeva8@gmail.com>
2025-07-17 13:48:48 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      {/* This file is typically used in Next.js App Router projects.
+          Since your project is primarily an HTML file, you can view the
+          full functionality by opening the 'index.html' file directly in the preview.
+          
+          If you were building a React/Next.js application, you would import
+          and render your main components here.
+       */}
+    </div>
+  )
+}
